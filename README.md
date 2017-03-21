4cbe9f9158940634350e9ca48c9e9726b1274790
isaranto <isarand@gmail.com>
2017-03-21 19:00:46 +0200

    [DEV] first commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# omega-index
+# omega_index
 Omega Index for evaluation of overlapping community structure
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'omega_index',
+  packages = ['omega_index'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Omega Index for evaluation of overlapping community structure',
+  author = 'Ilias Sarantopoulos',
+  author_email = 'sarantopoulos.ilias@gmail.com',
+  url = 'https://github.com/isaranto/omega-index', # use the URL to the github repo
+  download_url = 'https://github.com/isaranto/omega-index/archive/master.zip', # I'll explain this in a second
+  keywords = ['evaluation', 'clustering', 'community-detection'], # arbitrary keywords
+  classifiers = [],
+)
